a54c800a9674c254869fcc1a8482ee0f6db91788
DonDonu <Samsodienyusuf19@gmail.com>
2023-02-21 15:56:49 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,13 +15,13 @@ void print_to_98(int n)
 	{
 	if (n == 98)
 	{
-	printf("%d". n);
+	printf("%d", n);
 	printf("\n");
 	break;
 	}
 	else
 	{
-	printf("%d, ", n)
+	printf("%d, ", n);
 	}
 	}
 	}
@@ -37,7 +37,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-	printf("%d, ', n);
+	printf("%d", ', n);
 	}
 	}
 	}
