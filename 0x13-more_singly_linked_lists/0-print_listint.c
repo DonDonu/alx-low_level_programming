a40ecd7c863354a7598eaf8597eb7b736e489570
DonDonu <Samsodienyusuf20@gmail.com>
2023-04-04 11:12:47 -0700

    more linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,3 +1,4 @@
+
 #include "lists.h"
 
 /**
@@ -19,3 +20,4 @@ size_t print_listint(const listint_t *h)
 
 	return (num);
 }
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,4 +1,3 @@
-list.h
 #ifndef LISTS_H
 #define LISTS_H
 
@@ -15,8 +14,8 @@ list.h
  */
 typedef struct listint_s
 {
-	int n;
-	struct listint_s *next;
+ int n;
+ struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
